a49f20d2004e616be4e82edc7d916e3824f147b3
inder782 <inderchandel782@gmail.com>
2024-01-12 18:37:26 +0530

    day_2 user_authenticaton and database queries

--- user_management/app/auth/callback/route.ts
+++ user_management/app/auth/callback/route.ts
@@ -1,10 +1,12 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-
+import type { Database } from "@/lib/database.types";
 export async function GET(req: NextRequest) {
   const cookieStore = cookies();
-  const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
+  const supabase = createRouteHandlerClient<Database>({
+    cookies: () => cookieStore,
+  });
   const { searchParams } = new URL(req.url);
 
   const code = searchParams.get("code");
