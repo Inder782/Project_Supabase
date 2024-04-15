dab18abfc4163652bfab745d031751617a710a85
inder782 <inderchandel782@gmail.com>
2024-04-15 14:02:38 +0530

    feat(authentcation_v1):basic authentication using mock database

---
+++ express_backend/mock_data.ts
@@ -0,0 +1,10 @@
+export const users = [
+  {
+    id: 1,
+    username: "inder",
+  },
+  {
+    id: 2,
+    username: "inder2",
+  },
+];
--- express_backend/typescript_playground.ts
+++ express_backend/typescript_playground.ts
@@ -1,1 +1,9 @@
-let str: string = "HELLOO";
+// define the types which a variable can take
+
+let str: string = "inder"; // Defined that str will only take string
+let num: number = 54; // this will only take a number
+
+interface Person {
+  name: string;
+  age: number;
+}
